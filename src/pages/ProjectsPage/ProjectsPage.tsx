9d06dd79a91e995cffafa821cbb07cf21712529a
Joseph <iossif123gg@gmail.com>
2023-09-02 13:43:55 +0300

    create ProjectsPageWrapper

--- src/pages/ProjectsPage/ProjectsPage.tsx
+++ src/pages/ProjectsPage/ProjectsPage.tsx
@@ -1,14 +1,11 @@
 import { For } from "solid-js";
-import TableRow from "../../components/table/TableRow/TableRow";
-import TableWrapper from "../../components/table/TableWrapper/TableWrapper";
-import TableRowActionsMenu from "../../components/table/TableRow/TableRowActionsMenu";
-import { useModal } from "../../components/providers/ModalProvider";
-import FetchStatusIndicator from "./FetchStatusIndicator";
-import Heading from "../../components/ui/Heading";
-import EditableText from "../../components/form/EditableText/EditableText";
 import { updateProject } from "../../api";
+import EditableText from "../../components/form/EditableText/EditableText";
+import { useModal } from "../../components/providers/ModalProvider";
+import TableRow from "../../components/table/TableRow/TableRow";
+import TableRowActionsMenu from "../../components/table/TableRow/TableRowActionsMenu";
+import ProjectsPageWrapper from "./ProjectsPageWrapper";
 import { useProjectDeletion } from "./hooks/useProjectDeletion";
-import { useProjectsFetch } from "./hooks/useProjectsFetch";
 import { useProjectsActions } from "./hooks/useProjectsActions";
 import { useProjectsData } from "./hooks/useProjectsData";
 
@@ -16,8 +13,6 @@ const ProjectsPage = () => {
   const { projects, refetch } = useProjectsData();
   const { fetchProjectActions } = useProjectsActions(refetch);
   const { modalActions } = useModal();
-  const { isError, isLoading } = useProjectsFetch();
-
   const { handleDeleteProject } = useProjectDeletion();
 
   const handleSaveName = async (id: number, value: string) => {
@@ -26,32 +21,29 @@ const ProjectsPage = () => {
 
   return (
     <>
-      <Heading>Shown Projects: {projects().length}</Heading>
-      <FetchStatusIndicator isError={isError()} isLoading={isLoading()}>
-        <TableWrapper columns={["id", "name", "status", "actions"]}>
-          <For each={projects()}>
-            {({ id, attributes: { name, status } }) => (
-              <TableRow
-                rowElements={[
-                  id,
-                  <EditableText
-                    value={name}
-                    onSave={(value) => handleSaveName(id, value)}
-                  />,
-                  status,
-                  <TableRowActionsMenu
-                    onRequestDelete={() =>
-                      modalActions.open("delete", () =>
-                        handleDeleteProject(id, fetchProjectActions)
-                      )
-                    }
-                  />,
-                ]}
-              />
-            )}
-          </For>
-        </TableWrapper>
-      </FetchStatusIndicator>
+      <ProjectsPageWrapper heading={() => projects().length}>
+        <For each={projects()}>
+          {({ id, attributes: { name, status } }) => (
+            <TableRow
+              rowElements={[
+                id,
+                <EditableText
+                  value={name}
+                  onSave={(value) => handleSaveName(id, value)}
+                />,
+                status,
+                <TableRowActionsMenu
+                  onRequestDelete={() =>
+                    modalActions.open("delete", () =>
+                      handleDeleteProject(id, fetchProjectActions)
+                    )
+                  }
+                />,
+              ]}
+            />
+          )}
+        </For>
+      </ProjectsPageWrapper>
     </>
   );
 };
---
+++ src/pages/ProjectsPage/ProjectsPageWrapper.tsx
@@ -0,0 +1,28 @@
+// ProjectsPageWrapper.js
+
+import { ParentProps } from "solid-js";
+import TableWrapper from "../../components/table/TableWrapper/TableWrapper";
+import Heading from "../../components/ui/Heading";
+import FetchStatusIndicator from "./FetchStatusIndicator";
+import { useProjectsFetch } from "./hooks/useProjectsFetch";
+
+type Props = {
+  heading: () => number;
+} & ParentProps;
+
+const ProjectsPageWrapper = ({ children, heading }: Props) => {
+  const { isError, isLoading } = useProjectsFetch();
+
+  return (
+    <>
+      <Heading>{heading()}</Heading>
+      <FetchStatusIndicator isError={isError()} isLoading={isLoading()}>
+        <TableWrapper columns={["id", "name", "status", "actions"]}>
+          {children}
+        </TableWrapper>
+      </FetchStatusIndicator>
+    </>
+  );
+};
+
+export default ProjectsPageWrapper;
