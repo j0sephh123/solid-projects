848c7afebdb8b1e7d4248a5f308471fe6efbcf70
Joseph <iossif123gg@gmail.com>
2023-09-03 14:46:37 +0300

    add playwright boilerplate

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,21 +6,10 @@ import solidPlugin from "vite-plugin-solid";
 import devtools from "solid-devtools/vite";
 
 export default defineConfig({
-  plugins: [
-    /* 
-    Uncomment the following line to enable solid-devtools.
-    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
-    */
-    devtools(),
-    solidPlugin(),
-  ],
+  plugins: [devtools(), solidPlugin()],
   test: {
     environment: "jsdom",
     globals: true,
-    setupFiles: ["./tests/setup.js"],
-    // otherwise, solid would be loaded twice:
-    // if you have few tests, try commenting one
-    // or both out to improve performance:
     threads: false,
     isolate: false,
   },
